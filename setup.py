ff6cf059c1a7cd2378f51918a38b997730b0e748
PerceptronV <53554354+PerceptronV@users.noreply.github.com>
2019-08-15 16:04:00 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'PerceptronV',
   author_email = 'perceptronv@gmail.com',
   url = 'https://github.com/PerceptronV/vibra', #######
-  download_url = 'https://github.com/PerceptronV/vibra/archive/v1.0.tar.gz', ######
+  download_url = 'https://github.com/PerceptronV/vibra/archive/v1.1.tar.gz', ######
   keywords = ['Tensorflow', 'add-on'],
   install_requires=[
       'numpy',
